1f0f8952aabf1e62ea82789ed3736d6899c6e7e4
Oleg E. Vorobiov <isnullxbh@gmail.com>
2024-01-18 07:44:07 +0700

    Disable HTTPS redirection

--- src/xBh/Startup.fs
+++ src/xBh/Startup.fs
@@ -42,7 +42,6 @@ type Startup private () =
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts() |> ignore
 
-        app.UseHttpsRedirection() |> ignore
         app.UseMarkdown() |> ignore
         app.UseStaticFiles() |> ignore
 
